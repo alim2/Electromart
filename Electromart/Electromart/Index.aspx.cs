4ce3d5b334cfc7c5ae1160bddba675cf264e9458
Samiul Alim <shawon.1916@hotmail.com>
2015-11-06 15:49:37 +0600

    test 2

--- Electromart/Electromart/Index.aspx.cs
+++ Electromart/Electromart/Index.aspx.cs
@@ -11,7 +11,7 @@ namespace Electromart
     {
         protected void Page_Load(object sender, EventArgs e)
         {
-            Response.Write("");
+            Response.Write("Hello");
         }
     }
 }
